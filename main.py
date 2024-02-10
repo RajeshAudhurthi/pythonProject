68a5819f224b2575b68ee1159e02cdd27b30ad99
RajeshAudhurthi <raajesh404@gmail.com>
2024-02-10 06:17:27 +0530

    Added new python project

---
+++ main.py
@@ -0,0 +1,47 @@
+'''
+name = 'rajesh'
+age = 31
+height = 5.5
+indian = True
+
+name, age, height, indian = 'rajesh', 31, 5.5, True
+print(name+" is "+str(indian)+" indian"+" with age:"+str(age)+" years and height "+str(height)+'ft')
+
+audhurthi = 31
+rajesh = 31
+kumar = 31
+audhurthi= rajesh = kumar= 31
+
+print(audhurthi)
+print(rajesh)
+print(kumar)
+
+#############################
+
+name='raaJEsh'
+print(len(name))
+print(name.upper())
+print(name.lower())
+print(name.capitalize())
+print(name.find('h'))
+print(name.count("a"))
+print(name.isdigit())
+print(name.isalpha())
+print(name.replace('h','s'))
+print(name*7)
+
+#type casting = converting one data type to another data type
+x = 1 #int
+y =2.5 #float
+z="3" #str
+
+print(x)
+print(int(y))
+print(int(z)*2)
+
+'''
+######################
+
+name = input("what is your name - ")
+
+print(name)
